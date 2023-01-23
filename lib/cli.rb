cd06ff6ef72130ed030c4639b22f3ba36f4364c5
krsgarland <97922634+krsgarland@users.noreply.github.com>
2023-01-23 12:46:24 -0600

    defined signup method

--- lib/cli.rb
+++ lib/cli.rb
@@ -5,6 +5,22 @@ class CLI
         menu
     end
 
+    def menu
+        user_input = ""
+
+        while user_input != "exit"
+            puts "Request Oscar Award winning movies by year:"
+            puts "2015, 2014, 2013, 2012, 2011, 2010"
+            puts "Please enter a year:"
+
+            user_input = gets.chomp
+        
+            break if user_input == 'exit'
+
+            Scraper.scrape_and_print_movies(user_input)
+        end
+    end
+
     def signup_or_login
         user_input = ""
         while user_input !+ "exit"
@@ -34,20 +50,14 @@ class CLI
             end
         end
     end
-    
-    def menu
-        user_input = ""
 
-        while user_input != "exit"
-            puts "Request Oscar Award winning movies by year:"
-            puts "2015, 2014, 2013, 2012, 2011, 2010"
-            puts "Please enter a year:"
-
-            user_input = gets.chomp
-        
-            break if user_input == 'exit'
-
-            Scraper.scrape_and_print_movies(user_input)
-        end
+    def signup
+        puts "Please enter a username:"
+        username = gets.chomp
+        puts "Please enter a password:"
+        password = gets.chomp
+        puts "Sign up successful! Please login:"
+        User.new(username, password)
+        login
     end
 end
