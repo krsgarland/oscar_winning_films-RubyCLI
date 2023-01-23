2acccf62b24a9aa34f2c58256e4a1d3edb45ad9b
krsgarland <97922634+krsgarland@users.noreply.github.com>
2023-01-23 12:36:21 -0600

    require user.rb to CLI

--- lib/oscar_winning_films_cli.rb
+++ lib/oscar_winning_films_cli.rb
@@ -3,6 +3,7 @@
 require_relative "ruby_cli_project/version"
 require_relative 'cli.rb'
 require_relative 'scraper.rb'
+require_relative 'user.rb'
 
 module RubyCliProject
   class Error < StandardError; end
