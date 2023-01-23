428f6ac9bb4bc6b60792b1e78b8c9eed3c2c8f28
krsgarland <97922634+krsgarland@users.noreply.github.com>
2023-01-23 12:29:00 -0600

    Hash passwords & populate users

--- lib/user.rb
+++ lib/user.rb
@@ -4,13 +4,35 @@ require "pry"
 class User 
     @@users = []
     attr_accessor :username, :password
+
+    def self.hash_password(password)
+        BCrypt::Password.create(password)
+    end
+
     def initialize(username, password)
         @username = username
-        @password = password
+        @password = User.hash_password(password)
         @@users << self
     end
 
     def self.all 
         @@users
+    end
+
+    def self.seed 
+        users = [
+            {
+            username: "kristagarland",
+            password: "password",
+        },
+        {
+            username: "codefistudent",
+            password: "password2"
+        },
+        ]
+
+        users.each do |user|
+            User.new(user[:username], user[:password])
+        end
     end
 end
