5b9c4838e8ea1a99573a3aabaa9602c164bd04ea
krsgarland <97922634+krsgarland@users.noreply.github.com>
2023-01-23 12:35:10 -0600

    authenticate users

--- lib/user.rb
+++ lib/user.rb
@@ -5,18 +5,25 @@ class User
     @@users = []
     attr_accessor :username, :password
 
-    def self.hash_password(password)
-        BCrypt::Password.create(password)
-    end
-
     def initialize(username, password)
         @username = username
         @password = User.hash_password(password)
         @@users << self
     end
 
-    def self.all 
-        @@users
+    def self.authenticate_user(username, password)
+        authenticate_user = nil
+        @@users.each do |user|
+            if user.username == username && user.password == password
+                authenticate_user = user
+                break;
+            end
+        end
+        authenticate_user
+    end
+
+    def self.hash_password(password)
+        BCrypt::Password.create(password)
     end
 
     def self.seed 
@@ -34,5 +41,9 @@ class User
         users.each do |user|
             User.new(user[:username], user[:password])
         end
+    end
+    
+    def self.all 
+        @@users
     end
 end
